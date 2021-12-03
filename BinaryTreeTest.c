1eea876f204ef996cd94cdbb5f172d719f0e81b6
Gaein nidb <mail@gaein.cn>
2021-12-03 20:26:41 +0800

    add test

--- BinaryTreeTest.c
+++ BinaryTreeTest.c
@@ -1,4 +1,39 @@
-//
-// Created by Gaein_nidb on 2021/12/3.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "util.h"
+#include "BinaryTree.h"
 
+#define T char
+#define NODE_COUNT (10)
+
+void *getInput() {
+    T input = (T) getchar();
+
+    void *inputPtr = new(T);
+    memcpy(inputPtr, &input, sizeof(T));
+
+    return input == EOF ? NULL : (void *) inputPtr;
+}
+
+void *output(void *data) {
+    printf(" %c", *((T *) data));
+}
+
+int main() {
+    for (int i = NODE_COUNT; i > 0; --i) {
+        printf("%s", "Pre-order create binary tree, please input\n:");
+
+        BinaryTree *tree = createBinaryTree(getInput, sizeof(T));
+
+        printf("\n%s:\n", "Pre-order");
+        preOrderTree(tree, output);
+
+        printf("\n%s:\n", "In-order");
+        inOrderTree(tree, output);
+
+        printf("\n%s:\n", "Post-order");
+        postOrderTree(tree, output);
+    }
+    return 0;
+}
