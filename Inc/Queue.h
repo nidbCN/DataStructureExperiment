f196998f6643e7ccd0faed6793e4e45dcaf09125
Gaein nidb <mail@gaein.cn>
2021-12-20 23:20:56 +0800

    Add a queue

--- Inc/Queue.h
+++ Inc/Queue.h
@@ -1,8 +1,29 @@
-//
-// Created by Gaein_nidb on 2021/12/20.
-//
+#ifndef QUEUE_H
+#define QUEUE_H
 
-#ifndef DATASTRUCTURE_QUEUE_H
-#define DATASTRUCTURE_QUEUE_H
+#include <stddef.h>
 
-#endif //DATASTRUCTURE_QUEUE_H
+typedef struct queueNode {
+    void *data;
+    struct queueNode *prev;
+    struct queueNode *next;
+} QueueNode;
+
+typedef struct queue {
+    QueueNode *head;
+    QueueNode *tail;
+    int count;
+    size_t size;
+} Queue;
+
+#define create(T) (_create(sizeof(T)))
+
+Queue *_create(size_t size);
+
+void *dequeue(Queue *queue);
+
+void enqueue(Queue *queue, void *data);
+
+void clear(Queue *queue);
+
+#endif //QUEUE_H
--- Src/Queue.c
+++ Src/Queue.c
@@ -1,5 +1,62 @@
-//
-// Created by Gaein_nidb on 2021/12/20.
-//
-
+#include <memory.h>
 #include "Queue.h"
+#include "DataStructureUtil.h"
+
+Queue *_create(size_t size) {
+    Queue *newQueue = new(Queue);
+    newQueue->head = NULL;
+    newQueue->count = 0;
+    newQueue->size = size;
+    return newQueue;
+}
+
+void *dequeue(Queue *queue) {
+    QueueNode *node = queue->head;
+
+    queue->head = node->next;
+    queue->head->prev = NULL;
+
+    void *result = node->data;
+    node->prev = NULL;
+    node->next = NULL;
+    node->data = NULL;
+    free(node);
+
+    queue->count--;
+    return result;
+}
+
+void enqueue(Queue *queue, void *data) {
+    QueueNode *node = new(QueueNode);
+
+    memcpy(node->data, data, queue->size);
+    node->next = NULL;
+    node->prev = queue->tail;
+
+    queue->tail = node;
+
+    if (queue->count == 0) {
+        queue->head = node;
+    }
+
+    queue->count++;
+}
+
+void clear(Queue *queue) {
+    int i = 0;
+    QueueNode *ptr = queue->head;
+    while (i < queue->count) {
+        QueueNode *this = ptr;
+        ptr = ptr->next;
+        ++i;
+
+        this->next = NULL;
+        this->prev = NULL;
+        this->data = NULL;
+        free(this);
+    }
+
+    queue->head = NULL;
+    queue->tail = NULL;
+    free(queue);
+}
