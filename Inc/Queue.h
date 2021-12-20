09cdd2138a8e6f3d562370b3705cb615b6249ca1
Gaein nidb <mail@gaein.cn>
2021-12-20 22:13:30 +0800

    Add queue

---
+++ Inc/Queue.h
@@ -0,0 +1,8 @@
+//
+// Created by Gaein_nidb on 2021/12/20.
+//
+
+#ifndef DATASTRUCTURE_QUEUE_H
+#define DATASTRUCTURE_QUEUE_H
+
+#endif //DATASTRUCTURE_QUEUE_H
---
+++ Src/Queue.c
@@ -0,0 +1,5 @@
+//
+// Created by Gaein_nidb on 2021/12/20.
+//
+
+#include "Queue.h"
